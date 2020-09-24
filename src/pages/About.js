017dee0baf3c3b4a54d48538875513e68d883917
Tevinisaac <isaactevin@yahoo.com>
2020-09-24 21:08:11 +0300

    Content Writing

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,6 +7,11 @@ const About = () => (
 	<div>
 		<Header />
 		<h1>About page content Writing</h1>
+		<h2>Zao Bora is an online trading platform that connects Consumers direct with the Farmers to ease communication and better accessability of fresh farm produce between both parties.
+			Through this platform you can now get any farm produce from a farmer  at  a retail price and a wholesale price straight to you at just a click of a button.
+			Everyone deserves fresh farm produce without a struggle.Live healthy and be the change of the future.Zao Bora ni Afya bora.
+		</h2>
+		
 		<MobileNav />
 	</div>
 );
